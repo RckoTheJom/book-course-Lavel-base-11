a09ed875177f36bcf512b6826f9e987d37295c52
Ronald <ronaldstiwart@hotmail.com>
2024-12-06 10:01:42 -0500

    Migraciones

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PrimerControlador;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', [PrimerControlador::class,'index']);
+Route::get('index2/{post}', [PrimerControlador::class,'index2']);
